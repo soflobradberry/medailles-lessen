0c2478618d554ddf9af7e1208520c35e229511bf
soflobradberry <bryan.kreutz.us@gmail.com>
2025-11-12 10:01:53 +0100

    project setuo done

---
+++ medailles opdracht/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace medailles_opdracht
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
